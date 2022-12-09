2fba15f3f0d3239c69e3ba827c06be120802a8d6
Nor_Book <thanawat.srit@kmutt.ac.th>
2022-12-09 16:20:08 +0700

    Add Program

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static CircularLinkedList<double> circ = new CircularLinkedList<double>();
+    static void Main(string[] args)
+    {
+        Input();
+        Check(int.Parse(Console.ReadLine()));
+    }
+    static void Input()
+    {
+        while (true)
+        {
+            double input = double.Parse(Console.ReadLine());
+            if (input > 0)
+            {
+                circ.Add(input);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    static void Check(int n)
+    {
+        for (int i = 0; i < circ.GetLength(); i++)
+        {
+            double sum = 0;
+            for (int j = i - n / 2; j < i + 1 + n / 2; j++)
+            {
+                sum += circ.Get(j) / Convert.ToDouble(n);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
